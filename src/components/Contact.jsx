b181bfd4dacacc25f79470b62206076366f0d45c
yadunandan1 <adarshy85852@gmail.com>
2026-01-15 21:25:34 +0530

    some changes in contact page

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -46,8 +46,8 @@ const Contact = () => {
     // Send email using EmailJS
     try {
       await emailjs.send(
-        import.meta.env.VITE_EMAILJS_SERVICE_ID,
-        import.meta.env.VITE_EMAILJS_TEMPLATE_ID,
+        "service_i7dylcb",
+        "template_ofwvr13",
         {
           fullName: formData.name,
           email: formData.email,
@@ -55,7 +55,7 @@ const Contact = () => {
           subject: formData.subject,
           message: formData.message,
         },
-        import.meta.env.VITE_EMAILJS_PUBLIC_KEY
+        "WIEXDFlFFPw3VCrN4"
       );
       
       setStatus({
